1bfb6dca08acd453c48483eb03163d43cf621c5f
MacimeG <maxime.glaneux@gmail.com>
2023-05-11 14:30:33 +0200

    mise en place du site, html css js natif

---
+++ main.js
@@ -0,0 +1,7 @@
+const main = document.querySelector('.main')
+
+
+const paragraphe = document.createElement('p')
+paragraphe.innerHTML="bienvenue a tous sur mon futur portofolio"
+
+main.appendChild(paragraphe)
